10bdb582f480dafa75cffe772766d3635ddf39aa
marianosimone <marianosimone@gmail.com>
2013-01-09 14:46:20 -0300

    CLEANUP remove empty lines, commented out code, etc

--- src/test/java/org/mule/modules/AvalaraTestDriver.java
+++ src/test/java/org/mule/modules/AvalaraTestDriver.java
@@ -237,8 +237,6 @@ public class AvalaraTestDriver {
 
     @Test
     public void validateAValidAddress() throws Exception {
-
-
         ValidateResult response = module.validateAddress(account, license, client, "435 Ericksen Ave", null, null, null, "NE", null,
                 "98110", null, 0, null, null, TextCaseType.DEFAULT, false, false, testDate);
 
@@ -258,8 +256,6 @@ public class AvalaraTestDriver {
     public void saveBatchFile() throws Exception {
         String fileString = "ProcessCode,DocCode,DocType,DocDate,CompanyCode,CustomerCode,EntityUseCode,LineNo,TaxCode,TaxDate,ItemCode,Description,Qty,Amount,Discount,Ref1,Ref2,ExemptionNo,RevAcct,DestAddress,DestCity,DestRegion,DestPostalCode,DestCountry,OrigAddress,OrigCity,OrigRegion,OrigPostalCode,OrigCountry,LocationCode,SalesPersonCode,PurchaseOrderNo,CurrencyCode,ExchangeRate,ExchangeRateEffDate,PaymentDate,TaxIncluded,DestTaxRegion,OrigTaxRegion,Taxable,TaxType,TotalTax,CountryName,CountryCode,CountryRate,CountryTax,StateName,StateCode,StateRate,StateTax,CountyName,CountyCode,CountyRate,CountyTax,CityName,CityCode,CityRate,CityTax,Other1Name,Other1Code,Other1Rate,Other1Tax,Other2Name,Other2Code,Other2Rate,Other2Tax,Other3Name,Other3Code,Other3Rate,Other3Tax,Other4Name,Other4Code,Other4Rate,Other4Tax,ReferenceCode\n" +
                 "3,INV00000137,3,2012-11-06,TC,A00000001,,2c92c0943aaaf3bf013add2b74db437a,,2012-11-06,Mule iON,,1,20,,2c92c0943aaaf3bf013add2b74db437a,2c92c0943aaaf3bf013add2b74c24378,,,30 Maiden Ln,San Francisco,California,94108-5429,United States,,,,,,,,,USD,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,";
-        //OutputStream outputStream =   convertStringtoStream(fileString);
-
         System.out.print(fileString);
 
         BatchSaveResult saveResult = module.saveBatch("emiliano.lesende@mulesource.com", "muleftw1", client, BatchType.TRANSACTION_IMPORT, 156785, fileString, "filenr004");
@@ -279,47 +275,32 @@ public class AvalaraTestDriver {
             System.out.println("Estimated Completion = " + saveResult.getEstimatedCompletion());
 
         }
-
-
     }
 
     @Test
     public void fetchBatch() throws Exception {
 
-
         //BatchFileFetchResult fetchResult = module.fetchBatchFile(account, license, client, "129829");
-
         Map<String, BatchFileFetchResult> fetchResult = module.fetchBatchFile(account, license, client, "129275");
 
         System.out.println("result size= " + fetchResult.get("result").getBatchFiles().getBatchFile().size());
         System.out.println(new String(fetchResult.get("result").getBatchFiles().getBatchFile().get(0).getContent()));
-
 
         if (fetchResult.get("error") != null) {
             System.out.println("result size= " + fetchResult.get("error").getBatchFiles().getBatchFile().size());
             System.out.println(new String(fetchResult.get("error").getBatchFiles().getBatchFile().get(0).getContent()));
         }
 
-
         //System.out.println("Batches = " + new String(fetchResult.getBatches().getBatch().get(0).getFiles().getBatchFile().get(0).getContent()));
-
     }
 
     @Test
     public void isBatchFinished() throws Exception {
-
-
         //BatchFileFetchResult fetchResult = module.fetchBatchFile(account, license, client, "129829");
-
         boolean isFinished = module.isBatchFinished(account, license, client, "129275");
 
         System.out.println("isFinished= " + isFinished);
-
-
-
-
         //System.out.println("Batches = " + new String(fetchResult.getBatches().getBatch().get(0).getFiles().getBatchFile().get(0).getContent()));
-
     }
 
 }
